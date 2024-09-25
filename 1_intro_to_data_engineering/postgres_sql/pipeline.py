1f6b304bb14b9e0b15e6d6c84a778d579f0cf9ea
kkumyk <k.kumykova@gmail.com>
2024-09-25 18:40:32 +0100

    intro to data engineering and docker set up

---
+++ 1_intro_to_data_engineering/postgres_sql/pipeline.py
@@ -0,0 +1,16 @@
+import sys
+import pandas
+
+# print arguments
+print(sys.argv)
+
+# argument 0 is the name os the file
+
+# argument 1 contains the actual first argument
+nr_o_pizzas = sys.argv[1]
+
+# your pandas code:
+
+
+# print a sentence with the argument
+print(f'Job finished successfully! Celebrate it with {nr_o_pizzas} pizzas!')
